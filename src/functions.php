2e7ae1743728594782f05abaf7d52ba9f6e09993
Rayhan Hanaputra <hanaputrarayhan@gmail.com>
2024-02-05 22:05:31 +0700

    run test case

--- src/functions.php
+++ src/functions.php
@@ -4,7 +4,7 @@ function pdo_connect()
 {
     $DATABASE_HOST = 'mysql';
     $DATABASE_USER = 'root';
-    $DATABASE_PASS = 'root';
+    $DATABASE_PASS = 'password';
     $DATABASE_NAME = 'badcrud';
     try {
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
